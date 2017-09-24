032813e554ce1444baaa67f9d251b42dcf7a1813
lhy <liuhongyand@gmail.com>
2017-09-24 09:57:59 +0800

    angular_test 指令初试版

---
+++ js/directive&controller.js
@@ -0,0 +1,24 @@
+var myModule=agular.module('myModule',[]);
+myModule.controller('myCtrl',['$scope',function($scope){
+	$scope.loadData=function(){
+		console.log('数据加载中。。。')；
+	};
+
+}]);
+myModule.controller('myCtr2',['$scope',function($scope){
+	$scope.loadData2=function(){
+		console.log('数据加载中。。。2222')；
+	};
+
+}]);
+myModule.directive('loader', function(){
+	// Runs during compile
+	return {
+		restrict:'AE';
+		link: function($scope, iElm, iAttrs, controller) {
+			iElm.bind('mouseenter',function(event){
+				$scope.$apply(iAttrs.howtoload);
+			})
+		}
+	};
+});
